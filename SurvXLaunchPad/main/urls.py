c6f41d60fbf2cacae94efe52d96156812c9ba481
SurvX <95054133+SurvX@users.noreply.github.com>
2023-06-26 15:16:03 -0400

    Update urls.py

--- SurvXLaunchPad/main/urls.py
+++ SurvXLaunchPad/main/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('projects', views.projects, name='projects'),
+    path('products', views.products, name='products'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('portfolio', views.portfolio, name='portfolio'),
     path('forum', views.forum, name='forum'),
     path('aboutus', views.aboutus, name='aboutus'),
-    path('launchedprojects', views.launchedprojects, name='launchedprojects'),
+    path('marketplace', views.marketplace, name='marketplace'),
     path('upcomingprojects', views.upcomingprojects, name='upcomingprojects'),
-    path('launchnewprojects', views.launchnewprojects, name='launchnewprojects'),
+    path('daoproposals', views.daoproposals, name='daoproposals'),
 ]
